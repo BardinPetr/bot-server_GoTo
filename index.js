dc8ae4f2245e3d96c5daa1a9f2b8127dd183561f
BardinPetr <bardin.petr@gmail.com>
2017-03-10 08:45:29 +0000

    Some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = require("./server.js").Server;
 server = new server("");
 
 var bot = require("./bot.js").Bot;
-bot = new bot("");
+bot = new bot("mongodb://infinity-cat.ddnsking.com:27017/goto_bot");
 
 server.start();
 bot.start();
